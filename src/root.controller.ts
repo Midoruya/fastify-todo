83687f7586db8362a989d11399b4b369c202d138
Midoriya <wladimi482002@gmail.com>
2023-09-13 21:16:02 +0300

    init project

---
+++ src/root.controller.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance } from "fastify"
+
+export default async function (app: FastifyInstance) {
+    console.log('init');
+    
+  app.get('/', () => {
+    return { something: 'else' }
+  })
+
+}
