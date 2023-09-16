d728a04420c1540117ac5a289c9e99ad6fffe99e
Midoriya <wladimi482002@gmail.com>
2023-09-16 20:28:19 +0300

    feat(swagger): Добавлен swagger

--- src/app.ts
+++ src/app.ts
@@ -11,16 +11,13 @@ async function buildApp(options: AppOptions = {}) {
   const fastify = Fastify(options);
   fastify.register(envPlugin);
   fastify.register(prismaPlugin)
-  fastify.register(autoLoad, {
-    dir: path.join(__dirname),
-    dirNameRoutePrefix: function (folderParent: string, folderName: string) {
-      console.log(folderParent, folderName);
-      return true;
-    },
-    indexPattern: /.*controller.(?:js|ts)/,
-    ignorePattern: /.*(?:test|spec).(?:js|ts)/
-  });
   fastify.register(swaggerPlugin);
+  fastify.register(autoLoad, {
+    dir: path.join(__dirname, 'controller'),
+    dirNameRoutePrefix: (folderParent: string, folderName: string) => folderName.toLowerCase(), 
+    indexPattern: /.*controller.(?:js|ts)/,
+    ignorePattern: /.*(?:test|spec|plugin).(?:js|ts)/
+  });
   return fastify;
 }
 
--- src/plugin/swagger.plugin.ts
+++ src/plugin/swagger.plugin.ts
@@ -9,7 +9,6 @@ const swaggerOptions = {
             description: "My Description.",
             version: "1.0.0",
         },
-        host: "localhost",
         schemes: ["http", "https"],
         consumes: ["application/json"],
         produces: ["application/json"],
